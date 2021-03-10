92d5bb93f93c450db6930f69250abc0613ba3d3b
Darragh <g00352047@gmit.ie>
2021-03-10 10:11:04 +0000

    Created constructor method and tests for invalid data

--- src/main/java/ie/gmit/Employee.java
+++ src/main/java/ie/gmit/Employee.java
@@ -14,6 +14,29 @@ public class Employee {
         this.age = 35;
     }
 
+    public Employee(String t, String n, String pID, String pN, String e, int a) {
+        if(t != "Mr" && t != "Mrs" && t != "Miss")
+            throw new IllegalArgumentException("Not a valid Title");
+        if(n.length() < 5 || n.length() > 22)
+            throw new IllegalArgumentException("Name provided is not valid");
+        if(pID.length() != 6)
+            throw new IllegalArgumentException("Employee PPS should be 6 characters");
+        if(pN.length() != 7)
+            throw new IllegalArgumentException("Employee phone number should be 7 numbers");
+        if(e != "Full-time" && e != "Part-time")
+            throw new IllegalArgumentException("Employee should be Full-time or Part-time");
+        if(a < 18)
+            throw new IllegalArgumentException("Employee age must be over 18");
+        else {
+            this.title = t;
+            this.name  = n;
+            this.ppsID = pID;
+            this.phone = pN;
+            this.employmentType = e;
+            this.age = a;
+        }
+    }
+
     public String getTitle() {
         return title;
     }
--- src/test/java/ie/gmit/EmployeeTest.java
+++ src/test/java/ie/gmit/EmployeeTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class EmployeeTest {
     private static String fullTime = "Full-time", partTime = "Part-time";
@@ -22,6 +23,63 @@ public class EmployeeTest {
         assertEquals("0123456", employee.getPhone());
         assertEquals("Full-time", employee.getEmploymentType());
         assertEquals(35, employee.getAge());
+    }
+
+
+    @Test
+    public void testValidEmployee() {
+        employee = new Employee("Miss", "Regina Tower", "987654", "9123456", partTime, 18);
+        assertEquals("Miss", employee.getTitle());
+        assertEquals("Regina Tower", employee.getName());
+        assertEquals("987654", employee.getPPSID());
+        assertEquals("9123456", employee.getPhone());
+        assertEquals(partTime, employee.getEmploymentType());
+        assertEquals(18, employee.getAge());
+    }
+
+    @Test
+    public void testEmployeeWithInvalidTitleValues() {
+        Exception e = assertThrows(IllegalArgumentException.class, ()->{new Employee("Master", "Joe Bloggs", "123456", "0123456", fullTime, 35);});
+        assertEquals("Not a valid Title", e.getMessage());
+    }
+
+    @Test
+    public void testEmployeeWithInvalidNameValues() {
+        Exception e = assertThrows(IllegalArgumentException.class, ()->{new Employee("Mrs", "Joe", "12345", "0123456", fullTime, 35);});
+        assertEquals("Name provided is not valid", e.getMessage());
+
+        e = assertThrows(IllegalArgumentException.class, ()->{new Employee("Mr", "Honorificabilitudinitatibus", "12345", "0123456", fullTime, 35);});
+        assertEquals("Name provided is not valid", e.getMessage());
+    }
+
+    @Test
+    public void testEmployeeWithInvalidPPSIDValues() {
+        Exception e = assertThrows(IllegalArgumentException.class, ()->{new Employee("Miss", "Joe Bloggs", "12345", "0123456", fullTime, 35);});
+        assertEquals("Employee PPS should be 6 characters", e.getMessage());
+
+        e = assertThrows(IllegalArgumentException.class, ()->{new Employee("Mr", "Joe Bloggs", "1234567", "0123456", fullTime, 35);});
+        assertEquals("Employee PPS should be 6 characters", e.getMessage());
+    }
+
+    @Test
+    public void testEmployeeWithInvalidPhoneNumberValues() {
+        Exception e = assertThrows(IllegalArgumentException.class, ()->{new Employee("Mr", "Joe Bloggs", "123456", "012345", fullTime, 35);});
+        assertEquals("Employee phone number should be 7 numbers", e.getMessage());
+
+        e = assertThrows(IllegalArgumentException.class, ()->{new Employee("Mr", "Joe Bloggs", "123456", "01234567", fullTime, 35);});
+        assertEquals("Employee phone number should be 7 numbers", e.getMessage());
+    }
+
+    @Test
+    public void testEmployeeWithInvalidEmploymentValues() {
+        Exception e = assertThrows(IllegalArgumentException.class, ()->{new Employee("Mr", "Joe Bloggs", "123456", "0123456", "Part", 35);});
+        assertEquals("Employee should be Full-time or Part-time", e.getMessage());
+    }
+
+    @Test
+    public void testEmployeeWithInvalidAgeValues() {
+        Exception e = assertThrows(IllegalArgumentException.class, ()->{new Employee("Mr", "Joe Bloggs", "123456", "0123456", fullTime, 15);});
+        assertEquals("Employee age must be over 18", e.getMessage());
     }
 
 }
